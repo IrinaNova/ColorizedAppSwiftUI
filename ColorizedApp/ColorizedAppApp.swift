15ae3f3b187da55de6503bf4d9a9c742b82c0dd6
IrinaNova <irishaivashenok@mail.ru>
2023-01-14 19:26:34 +0300

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Irunchik on 14.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
